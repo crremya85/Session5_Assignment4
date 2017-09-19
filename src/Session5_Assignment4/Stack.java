74075ab26bdd124ffe7d8d38a6f30454d8d3c077
Remya C R <crremya85@gmail.com>
2017-09-19 23:49:21 +0530

    Session5_Assignment4

---
+++ src/Session5_Assignment4/Stack.java
@@ -0,0 +1,8 @@
+package Session5_Assignment4;
+//Interface having two methods push & pop. This interface will be extended by FixedStack.java and DynamicStack.java
+public interface Stack {
+
+	void push(int l);
+	int pop();
+	
+}
